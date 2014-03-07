094b89688b951c7a81c329c696c28f29fb7f9dd0
foriacus <634844533@qq.com>
2014-03-07 16:44:35 +0800

    update button text.

--- template.js
+++ template.js
@@ -1,1 +1,1 @@
-module.exports = '<div class="confirmation-popover-content">\n  <div class="confirmation-popover-message"></div>\n  <div class="confirmation-popover-actions">\n    <button class="cancel">Cancel</button>\n    <button class="ok main">Ok</button>\n  </div>\n</div>';
+module.exports = '<div class="confirmation-popover-content">\n  <div class="confirmation-popover-message"></div>\n  <div class="confirmation-popover-actions">\n    <button class="cancel">取消</button>\n    <button class="ok main">确定</button>\n  </div>\n</div>';
